d3c7401a7f2b095d13ce20f769c4bc66b826ea68
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2025-07-23 17:47:20 +0200

    update

--- _build.R
+++ _build.R
@@ -2,7 +2,7 @@
 
 # Compiler le fichier calendrier.qmd vers calendrier.html
 if (file.exists("data/insee/calendrier.qmd")) {
-  message("📄 Compilation de data/insee/calendrier.qmd")
+  message("📄 Compilation de: data/insee/calendrier.qmd")
   quarto::quarto_render(input = "data/insee/calendrier.qmd")
 } else {
   message("❌ Fichier data/insee/calendrier.qmd introuvable")
