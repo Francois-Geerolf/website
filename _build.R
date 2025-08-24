56602407cfe1f98926bcc4f1ea48de255a0912ad
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2025-08-24 15:40:43 -0700

    update

--- _build.R
+++ _build.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+source(here::here("_rinit.R"))
 
 # Fonction utilitaire pour formater un temps en minutes + secondes
 formater_temps <- function(secondes) {
@@ -7,75 +8,14 @@ formater_temps <- function(secondes) {
   sprintf("%dm %02ds", minutes, sec)
 }
 
-# Update OECD Databases
-base_path <- here::here("data", "oecd")
-scripts   <- list.files(base_path, pattern = "\\.R$", full.names = TRUE)
-
-# Global timer
-global_start <- Sys.time()
-
-for (script in scripts) {
-  if (file.exists(script)) {
-    start <- Sys.time()
-    tryCatch({
-      source(script, local = TRUE)
-      elapsed <- difftime(Sys.time(), start, units = "secs")
-      message("✅  Sourced: ", basename(script), 
-              " (", formater_temps(elapsed), ")")
-    }, error = function(e) {
-      message("❌  Failed: ", basename(script), 
-              " — ", e$message)
-    })
-  } else {
-    message("❌  File not found: ", basename(script))
-  }
-}
-
-# Global elapsed time
-global_elapsed <- difftime(Sys.time(), global_start, units = "secs")
-message("\n⏱️  Total time: ", formater_temps(global_elapsed))
-
-# Aller à la racine du dépôt GitHub
-# setwd(Sys.getenv("GITHUB_WORKSPACE"))
-
-# Fonction pour compiler un fichier .qmd, s’il existe, avec mesure du temps
-compiler_qmd <- function(fichier) {
-  if (file.exists(fichier)) {
-    message("📄 Compilation de ", fichier)
-    start <- Sys.time()
-    ok <- tryCatch({
-      quarto::quarto_render(input = fichier, quiet = quiet)
-      TRUE
-    }, error = function(e) {
-      message("❌ Erreur compilation : ", e$message)
-      FALSE
-    })
-    end <- Sys.time()
-    elapsed_sec <- as.numeric(difftime(end, start, units = "secs"))
-    if (ok) {
-      message("✅ Succès (", formater_temps(elapsed_sec), ")")
-      return(list(success = TRUE, time = elapsed_sec))
-    } else {
-      return(list(success = FALSE, time = NA))
-    }
-  } else {
-    message("❌ Fichier ", fichier, " introuvable")
-    return(list(success = FALSE, time = NA))
-  }
-}
-
 # Trouver tous les fichiers .qmd dans le dossier "data" et ses sous-dossiers
 # setwd("data")
 fichiers <- list.files(
-  path = "data",
+  path = ".",
   pattern = "\\.qmd$", 
-  recursive = TRUE, 
+  recursive = F, 
   full.names = TRUE
 )
-
-# Move "./themes.qmd" to last
-fichiers <- c(setdiff(fichiers, "data/themes.qmd"), "data/themes.qmd")
-fichiers <- c(setdiff(fichiers, "data/index.qmd"), "data/index.qmd")
 
 # Mesure du temps total
 debut_total <- Sys.time()
