731891000511568b552adfead8d6affa857fb4b3
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2026-02-19 17:02:41 +0100

    update

--- data/meteo/station_list.R
+++ data/meteo/station_list.R
@@ -1,6 +1,6 @@
-library("data.table")
-library("tidyverse")
-setwd("~/iCloud/website/data/meteo")
+
+source(here::here("_rinit.R"))
+setwd(here::here("data", "meteo"))
 
 # 
 # https://donneespubliques.meteofrance.fr/client/document/20140701_liste_stations_temps_reel_141_154.xls
@@ -10,6 +10,7 @@ curl::curl_download("https://donneespubliques.meteofrance.fr/client/document/201
 
 
 station_list <- readxl::read_xls("20140701_liste_stations_temps_reel_141_154.xls", skip = 9) %>%
-  slice(4:n())
+  slice(4:n()) %>%
+  as_tibble()
 
 save(station_list, file = "station_list.RData")
